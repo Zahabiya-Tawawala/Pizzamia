c83a3578d38d19ab14fc86e739395c6aeb59cfbc
Zahabiya Tawawala <zahabiyatawawala1@gmail.com>
2024-08-02 13:08:19 +0530

    basic setup for the app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Logo from './assets/Logo.png';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+    <div style={{display: 'flex'}}>
+    <img src={Logo} alt="" style={{
+          width: '100px',
+          height: '100px',
+          marginTop: '10px'
+        }}/>
+        <h1>Pizzamia App</h1>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
